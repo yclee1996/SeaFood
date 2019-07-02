c9623126665f222d76d290d6cec15490df2073a3
Jessica Lee <yuchi.lee0418@gmail.com>
2019-07-02 11:52:49 +0800

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by yuchi.lee on 2019/7/2.
+//  Copyright © 2019 Yuchi Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
